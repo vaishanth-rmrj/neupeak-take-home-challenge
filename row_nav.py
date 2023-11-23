7189ad99f87cb2a974d77f30f50c2e591f77cb18
vaishanth_r <vaishanth.rmrj@gmail.com>
2023-11-22 19:29:02 -0500

    implemented a class that will have necessary methods

--- row_nav.py
+++ row_nav.py
@@ -1,37 +1,30 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-'''
-    Test Description:
+class PathOrientationDetector:
+    def __init__(self, pointcloud_data_path, skip_factor=20) -> None:
+        self.pcl_data = self.load_point_cloud(pointcloud_data_path, skip_factor)        
 
-    [TASK 1]
-    Create an algorithm that takes in the following:
-        row_pointcloud : scanned pointcloud (3D numpy array) for the row from depth camera
+    def load_point_cloud(self, file_path, skip_factor):
+        """
+        file: [string]: npz file to be processed
+        return: pointcloud in numpy array
+        """
+        row_np_array = np.load(file_path)
+        row_pointcloud = row_np_array['arr_0']
 
-    And outputs the following:
-        angular_rate : rate in deg/s the robot should turn at such that it will centre itself in the row
-	
-	The goal is to genereate a target angular rate that the robot uses to turn. Positive rate means robot
-	will turn right, negative rate means robot will turn left. Assume the robot moves forward at some constant velocity
-	set by the user. Your algorithm then generates angular rates for the robot while it moves through the row. The ideal angular 
-	rate will make it so that the robot is always centred in the row. When the robot is perfectly centred the angular rate should be 0
-	since we just need the robot to go straight.
+        # down sampling point cloud for better performance
+        return row_pointcloud[::skip_factor, :]
 
-    [TASK 2]
-    Implement a ROS Node that will output correction anglular rate for the robot. Publish cmd_vel to a twist
-	ROS topic
-
-    [TASK 3]
-    unit-test: Write a unit test script using standard Python unittest library
-
-    [OPTIONAL BONUS 1]
-    end_of_row: Boolean parameter that is True if end of row is detected
-
-    
-
-
-    Evaluation Criteria: Method runtime. Lower is better.
-'''
+    def visualize_pcl(self, pcl_data):
+        """
+        visualize pcl using matplotlib
+        """
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.view_init(azim=0, elev=-180)
+        ax.scatter(pcl_data[:, 2], pcl_data[:, 0], pcl_data[:, 1])
+        plt.show()
 
 
 def plot_row_pointcloud(file):
@@ -52,4 +45,6 @@ def plot_row_pointcloud(file):
 
 
 if __name__ == "__main__":
-    pointcloud = plot_row_pointcloud("3.npz")
+    # pointcloud = plot_row_pointcloud("1.npz")
+    orientation_detector = PathOrientationDetector("1.npz")
+    orientation_detector.visualize_pcl(orientation_detector.pcl_data)
