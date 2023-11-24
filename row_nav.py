059a032db8e2ab14f23ab082c412201bd9c34462
vaishanth_r <vaishanth.rmrj@gmail.com>
2023-11-23 21:56:00 -0500

    updated PathOrientationDetector and added method to check path ending

--- row_nav.py
+++ row_nav.py
@@ -3,61 +3,51 @@ import matplotlib.pyplot as plt
 import open3d as o3d
 from sklearn.cluster import KMeans
 
-DEBUG = True
+DEBUG = False
 
 class PathOrientationDetector:
-    def __init__(self, pointcloud_data_path, voxel_size=0.01) -> None:
-        self.pcl_data = self.load_point_cloud(pointcloud_data_path, voxel_size)        
+    def __init__(self, 
+                 distance_threshold=0.01, 
+                 height_threshold=-0.1,
+                 is_pcl_downsample=False,  
+                 voxel_size=0.01) -> None:
+        
+        self.pcl_data = np.ndarray([])
+        self.l_wall_pcl, self.r_wall_pcl = np.ndarray([]), np.ndarray([])
+        self.ground_pcl = np.ndarray([])
 
-    def load_point_cloud(self, file_path, voxel_size):
-        """
-        load point cloud from pickle file and 
-        downsampling using open3d
+        # params
+        self.distance_threshold = distance_threshold
+        self.height_threshold = height_threshold
+        self.voxel_size = voxel_size
+        self.is_pcl_downsample = is_pcl_downsample
 
-        Arguments:
-            file_path: path to pickle file
-            skip_factor: value to skip points
+        # variables
+        self.is_path_ending = False
+        self.path_deviation_angle = 0  
 
-        Returns:
-            row_pointcloud: point cloud from file downsampled
-        """
-        row_np_array = np.load(file_path)
-        row_pointcloud = row_np_array['arr_0']
+    def set_pcl_from_array(self, pcl_array):
+        try:
+            if not isinstance(pcl_array, np.ndarray):
+                raise ValueError("Invalid point cloud type")
+                
+            if self.is_pcl_downsample:
+                # downsample pcl for better performance
+                pcd = o3d.geometry.PointCloud()
+                pcd.points = o3d.utility.Vector3dVector(pcl_array)
+                downpcd = pcd.voxel_down_sample(self.voxel_size) 
+                self.pcl_data = np.asarray(downpcd.points)    
+            else:
+                self.pcl_data = pcl_array
 
-        # down sampling point cloud for better performance
-        pcd = o3d.geometry.PointCloud()
-        pcd.points = o3d.utility.Vector3dVector(row_pointcloud)
-        # downpcd = pcd.voxel_down_sample(voxel_size)        
-        return np.asarray(pcd.points)
+            # segment pcl to walls and ground
+            self.l_wall_pcl, self.r_wall_pcl, self.ground_pcl = self.pcl_segmentation_open3d(self.pcl_data, 
+                                                                                            self.distance_threshold, 
+                                                                                            self.height_threshold)
+        except ValueError as ve:
+            print(f"Error: {ve}")    
 
-    def visualize_pcl(self, pcl_data):
-        """
-        visualize pcl data using matplotlib
-        Arguments:
-            pcl_data: point cloud data
-        """
-        fig = plt.figure()
-        ax = fig.add_subplot(111, projection='3d')
-        ax.view_init(azim=0, elev=-180)
-        ax.scatter(pcl_data[:, 2], pcl_data[:, 0], pcl_data[:, 1], c="green")
-        plt.show()
-
-    def visualize_pcl_segments(self, wall_pcl, ground_pcl):
-        """
-        visualize pcl segments using matplotlib
-        Arguments:
-            wall_pcl: point cloud data for wall points
-            ground_pcl: point cloud data for ground points        
-        """
-        fig = plt.figure()
-        ax = fig.add_subplot(111, projection='3d')
-        ax.view_init(azim=0, elev=-180)
-        if wall_pcl.any():
-            ax.scatter(wall_pcl[:, 2], wall_pcl[:, 0], wall_pcl[:, 1], c="gray")
-        if ground_pcl.any():
-            ax.scatter(ground_pcl[:, 2], ground_pcl[:, 0], ground_pcl[:, 1], c="brown")
-        plt.show()
-    
+       
     def pcl_segmentation_open3d(self, pcl_data, distance_threshold=0.01, height_threshold=-0.1):        
         """
         extract wall and ground segments from pcl
@@ -94,19 +84,26 @@ class PathOrientationDetector:
         # convert pcl to np array
         wall_pcl = np.asarray(outlier_cloud.points)
         wall_indices = np.where(wall_pcl[:, 1] < height_threshold)
-        wall_pcl = wall_pcl[wall_indices]        
-        return wall_pcl, np.asarray(inlier_cloud.points)
+        wall_pcl = wall_pcl[wall_indices]      
 
+        # kmeans clustering to separate left and right walls  
+        kmeans = KMeans(n_clusters=2)
+        labels = kmeans.fit_predict(wall_pcl)
+
+        # separate pcl into left wall and right wall clusters
+        left_wall_cluster = wall_pcl[labels == 0]
+        right_wall_cluster = wall_pcl[labels == 1]
+
+        return left_wall_cluster, right_wall_cluster, np.asarray(inlier_cloud.points)
 
     def determine_heading(self, pcl_data):
-        centroid = np.mean(pcl_data, axis=0)
         cov_matrix = np.cov(pcl_data.T)
         eigenvalues, eigenvectors = np.linalg.eig(cov_matrix)
 
         # Use the eigenvector corresponding to the highest eigenvalue as the principal axis
         principal_axis = eigenvectors[:, np.argmax(eigenvalues)]
 
-        # Determine heading (angle or orientation)
+        # Determine heading 
         heading_angle = np.arctan2(principal_axis[1], principal_axis[0])
         return np.degrees(heading_angle)
 
@@ -138,7 +135,7 @@ class PathOrientationDetector:
         principal_direction = eigenvectors[:, sorted_indices[0]]
         return principal_direction
 
-    def compute_heading_angle(self, pcl_data):
+    def compute_heading_angle(self):
         """
         computer heading angle using pcl data of walls
 
@@ -146,34 +143,27 @@ class PathOrientationDetector:
             pcl_data : Point cloud data of walls
         """
         # convert 3d pcl to 2d data points by removing y-axis
-        data_pts_2d = pcl_data[:, [0, 2]].copy()
-
-        # k-means clustering to segregate left and right wall pcl
-        kmeans = KMeans(n_clusters=2)
-        labels = kmeans.fit_predict(data_pts_2d)
-
-        # separate pcl into left wall and right wall clusters
-        left_cluster = data_pts_2d[labels == 0]
-        right_cluster = data_pts_2d[labels == 1]
+        left_data_pts = self.l_wall_pcl[:, [0, 2]].copy()
+        rigth_data_pts = self.r_wall_pcl[:, [0, 2]].copy()
 
         # compute heading direction of each wall
         # PCA to determine principal direction
-        l_principal_dir = self.compute_principal_direction(left_cluster)
-        r_principal_dir = self.compute_principal_direction(right_cluster)
+        l_principal_dir = self.compute_principal_direction(left_data_pts)
+        r_principal_dir = self.compute_principal_direction(rigth_data_pts)
         
         # computer heading angle using eigen values and vectors
         l_heading_angle = 90.0 - (self.get_heading_angle(l_principal_dir) * -1)
         r_heading_angle = 90.0 - (self.get_heading_angle(r_principal_dir) * -1)
 
         if DEBUG:
-            plt.scatter(left_cluster[:, 0], left_cluster[:, 1])
-            plt.quiver(np.mean(left_cluster[:, 0]), np.mean(left_cluster[:, 1]), 
+            plt.scatter(left_data_pts[:, 0], left_data_pts[:, 1])
+            plt.quiver(np.mean(left_data_pts[:, 0]), np.mean(left_data_pts[:, 1]), 
                     -l_principal_dir[0], -l_principal_dir[1], 
                     angles='xy', scale_units='xy', scale=0.5, 
                     color='red', label=f'Heading Angle: {l_heading_angle:.2f} degrees')
 
-            plt.scatter(right_cluster[:, 0], right_cluster[:, 1])
-            plt.quiver(np.mean(right_cluster[:, 0]), np.mean(right_cluster[:, 1]), 
+            plt.scatter(rigth_data_pts[:, 0], rigth_data_pts[:, 1])
+            plt.quiver(np.mean(rigth_data_pts[:, 0]), np.mean(rigth_data_pts[:, 1]), 
                     -r_principal_dir[0], -r_principal_dir[1], 
                     angles='xy', scale_units='xy', scale=0.1, 
                     color='green', label=f'Heading Angle: {r_heading_angle:.2f} degrees')
@@ -183,7 +173,78 @@ class PathOrientationDetector:
             plt.legend()
             plt.show()
         
-        return (l_heading_angle + r_heading_angle)/2.0     
+        self.path_deviation_angle = (l_heading_angle + r_heading_angle)/2.0   
+        return self.path_deviation_angle
+
+    def compute_bbox_area(self, bbox_pts):
+        """
+        compute surface area from 3D bounding box
+        Arguments:
+            bbox_pts (x, y, z): 3D points
+
+        Returns:
+            surface area of 3D bounding box
+        """
+        bbox_max_z = np.max(bbox_pts[:, 2])
+        bbox_min_z = np.min(bbox_pts[:, 2])
+        bbox_max_y = np.max(bbox_pts[:, 1])
+        bbox_min_y = np.min(bbox_pts[:, 1])
+        z_len = bbox_max_z - bbox_min_z
+        y_len = bbox_max_y - bbox_min_y
+        return z_len*y_len  
+    
+    def check_path_ending(self):
+        """
+        check if the path is ending my  measuring 
+        left and right wall area
+
+        Returns:
+            bool: True if path ending
+        """
+        left_pcd = o3d.geometry.PointCloud()
+        left_pcd.points = o3d.utility.Vector3dVector(self.l_wall_pcl)
+
+        right_pcd = o3d.geometry.PointCloud()
+        right_pcd.points = o3d.utility.Vector3dVector(self.r_wall_pcl)
+
+        # # Compute the axis-aligned bounding box
+        left_bbox = left_pcd.get_minimal_oriented_bounding_box()
+        right_bbox = right_pcd.get_minimal_oriented_bounding_box()
+        # Extract bounding box corners
+        left_bbox_corners = np.asarray(left_bbox.get_box_points())
+        right_bbox_corners = np.asarray(right_bbox.get_box_points())
+
+        l_wall_area = self.compute_bbox_area(left_bbox_corners)
+        r_wall_area = self.compute_bbox_area(right_bbox_corners)
+        return l_wall_area < 0.1 and r_wall_area < 0.1
+        
+    def visualize_pcl(self, pcl_data):
+        """
+        visualize pcl data using matplotlib
+        Arguments:
+            pcl_data: point cloud data
+        """
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.view_init(azim=0, elev=-180)
+        ax.scatter(pcl_data[:, 2], pcl_data[:, 0], pcl_data[:, 1], c="green")
+        plt.show()
+
+    def visualize_pcl_segments(self, wall_pcl, ground_pcl):
+        """
+        visualize pcl segments using matplotlib
+        Arguments:
+            wall_pcl: point cloud data for wall points
+            ground_pcl: point cloud data for ground points        
+        """
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.view_init(azim=0, elev=-180)
+        if wall_pcl.any():
+            ax.scatter(wall_pcl[:, 2], wall_pcl[:, 0], wall_pcl[:, 1], c="gray")
+        if ground_pcl.any():
+            ax.scatter(ground_pcl[:, 2], ground_pcl[:, 0], ground_pcl[:, 1], c="brown")
+        plt.show()
 
 def plot_row_pointcloud(file):    
     """
@@ -201,13 +262,28 @@ def plot_row_pointcloud(file):
 
     return row_pointcloud
 
+def load_point_cloud(file_path):
+        """
+        load point cloud from pickle file 
+        Arguments:
+            file_path: path to pickle file
+
+        Returns:
+            row_pointcloud: point cloud from file downsampled
+        """
+        row_np_array = np.load(file_path)
+        row_pointcloud = row_np_array['arr_0']
+        return row_pointcloud
+
 
 if __name__ == "__main__":
     # pointcloud = plot_row_pointcloud("1.npz")
 
-    detector = PathOrientationDetector("1.npz")
-    wall_pcl, ground_pcl = detector.pcl_segmentation_open3d(detector.pcl_data)
-    print(detector.compute_heading_angle(wall_pcl))
+    detector = PathOrientationDetector()
+    pcl_data = load_point_cloud("2.npz")
+    detector.set_pcl_from_array(pcl_data)
+    print(detector.compute_heading_angle())    
+    print(detector.check_path_ending()) 
 
     
 
