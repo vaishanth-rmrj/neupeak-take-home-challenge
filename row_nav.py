11ee1b09206f05317cddad2d6cd5142355e752d2
vaishanth_r <vaishanth.rmrj@gmail.com>
2023-11-24 10:15:01 -0500

    calc computation time

--- row_nav.py
+++ row_nav.py
@@ -33,13 +33,22 @@ def load_point_cloud(file_path):
         row_pointcloud = row_np_array['arr_0']
         return row_pointcloud
 
+import time
 if __name__ == "__main__":
-    detector = PathOrientationDetector()
-    pcl_data = load_point_cloud("2.npz")
+    start_time = time.time() 
+    detector = PathOrientationDetector(is_pcl_downsample=True)    
+    pcl_data = load_point_cloud("4.npz")   
+    
     detector.set_pcl_from_array(pcl_data)
+    
+    
     print("Robot heading angle: ", detector.compute_heading_angle())  
     print("Angualr rate of deviation: ", detector.compute_angular_correction_rate(5))    
     print("Is path ending: ", detector.check_path_ending()) 
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f"Time taken: {elapsed_time:.6f} seconds")
+    # Time taken: 0.026267 seconds/26.267milliseconds
 
     
 
