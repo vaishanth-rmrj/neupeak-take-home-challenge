3434b70eda092cb3bef90df7284fcf149b2970d8
vaishanth_r <vaishanth.rmrj@gmail.com>
2023-11-25 13:23:55 -0500

    added launch file

---
+++ ros2_ws/src/path_orientation_detector/launch/row_nav_node.launch.py
@@ -0,0 +1,40 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from ament_index_python.packages import get_package_share_directory
+import os
+import pkg_resources
+
+package_name = 'path_orientation_detector'
+
+def generate_launch_description():
+    pkg_name = 'path_orientation_detector'
+    # rviz_config_dir = os.path.join(
+    #     get_package_share_directory(pkg_name), 
+    #     'config', 
+    #     'rviz_config.rviz'
+    # )
+    rviz_config_dir = pkg_resources.resource_filename(pkg_name, 'config/rviz_config.rviz')
+
+    return LaunchDescription([                              
+        DeclareLaunchArgument('input_pcl_file', default_value='4.npz',
+                              description='input point cloud file to load'),        
+
+        Node(
+            package='path_orientation_detector',
+            executable='row_nav',
+            name='row_nav_node',
+            output='screen',
+            parameters=[{'input_pcl_file': LaunchConfiguration('input_pcl_file')}],
+            arguments=['--ros-args -p'],
+        ),        
+
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+            arguments=['-d', rviz_config_dir]
+        )
+    ])
