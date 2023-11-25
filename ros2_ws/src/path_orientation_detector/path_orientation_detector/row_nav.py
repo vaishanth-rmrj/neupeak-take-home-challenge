1687fbd9a578904b0867c27eedbcccf779e074bd
vaishanth_r <vaishanth.rmrj@gmail.com>
2023-11-25 11:32:29 -0500

    added and updated timer callback method

--- ros2_ws/src/path_orientation_detector/path_orientation_detector/row_nav.py
+++ ros2_ws/src/path_orientation_detector/path_orientation_detector/row_nav.py
@@ -1,4 +1,5 @@
 import numpy as np
+import time
 from .orientation_detector import PathOrientationDetector
 
 # ros2 imports
@@ -24,36 +25,60 @@ class PathOrientationNode(Node):
     def __init__(self):
         super().__init__('path_orientation_node')
         self.detector = PathOrientationDetector(is_pcl_downsample=True, voxel_size=0.02)
+        # publishers
         self.publisher_ = self.create_publisher(Twist, '/cmd_vel', 10)
         self.walls_pcl_pub = self.create_publisher(PointCloud2, '/point_cloud_walls', 10)
         self.ground_pcl_pub = self.create_publisher(PointCloud2, '/point_cloud_ground', 10)
-
-        self.timer = self.create_timer(1.0, self.publish_cmd_vel)
-        self.pcl_pub_timer = self.create_timer(1.0, self.publish_pcl)
-
         self.robot_heading_arrow_pub = self.create_publisher(Marker, '/robot_heading_arrow', 10)
         self.path_heading_arrow_pub = self.create_publisher(Marker, '/path_heading_arrow', 10)
-        self.create_timer(1.0, self.publish_arrow_marker)
+
+        # timer
+        self.create_timer(1.0, self.timer_callback)
+
+        # variables
+        self.path_deviation_angle = 0.0
+        self.is_path_ending = False
+        self.angular_correction_rate = 0.0
+        self.avg_compute_time = 0.0
+
+    def timer_callback(self):
+        """
+        execute required methods and publish as ros msgs
+        """
+        start_time = time.time() 
+        self.path_deviation_angle = self.detector.compute_heading_angle()
+        self.angular_correction_rate = self.detector.compute_angular_correction_rate(5)
+        self.is_path_ending = self.detector.check_path_ending()
+        self.avg_compute_time += (time.time() - start_time)
+        self.avg_compute_time /= 2
+        self.get_logger().info("-"*40)
+        self.get_logger().info("Path deviation angle (deg): "+str(round(self.path_deviation_angle, 3)))
+        self.get_logger().info("Angular correction rate (deg/s): "+str(round(self.angular_correction_rate, 3)))
+        self.get_logger().info("Path ending status: "+str(self.is_path_ending))
+        self.get_logger().info("Average compute time (ms): "+str(round(self.avg_compute_time*1000, 3)))
+
+        # publish data as ros msg
+        self.publish_cmd_vel()
+        self.publish_pcl()
+        self.publish_arrow_marker()         
 
     def publish_cmd_vel(self):
         """
         publish path correction angular rate(deg/s)
         through cmd_vel topic
         """
+        self.get_logger().info("Publishing cmd_vel ...")
         twist_msg = Twist()
-        twist_msg.linear.x = 0.2  # Change this value as needed
-        twist_msg.angular.z = 0.1  # Change this value as needed
+        twist_msg.linear.x = 0.1
+        twist_msg.angular.z = self.angular_correction_rate 
         self.publisher_.publish(twist_msg)
-        self.get_logger().info("Publishing to /cmd_vel")
-        self.get_logger().info(str(self.detector.compute_heading_angle()))
-        self.get_logger().info(str(self.detector.compute_angular_correction_rate(5)))
-        self.get_logger().info(str(self.detector.check_path_ending()))
     
     def publish_pcl(self):
         """
         publish walls and ground pcl data as 
         ROS2 PoinCloud2 msg type
         """
+        self.get_logger().info("Publishing segmented point cloud...")
         ground_pcl_msg = generate_pointcloud2_msg(pcl_data=self.detector.ground_pcl, 
                                                 pcl_fields=FIELDS_XYZ,
                                                 timestamp=self.get_clock().now().to_msg(),
@@ -71,14 +96,14 @@ class PathOrientationNode(Node):
         publish robot heading and path deviation angle as 
         ROS2 Marker msg type
         """
-        path_deviation_angle = self.detector.path_deviation_angle
+        self.get_logger().info("Publishing heading arrow marker...")
         robot_heading_arrow_msg = get_arrow_marker_msg(euler_heading_angle=(0, 0, 90), 
                                                     rgb_color=(1.0, 0.0, 0.0), 
                                                     timestamp=self.get_clock().now().to_msg(),
                                                     arrow_len=0.4, 
                                                     id=0,
                                                     frame_id="base_link") 
-        path_heading_arrow_msg = get_arrow_marker_msg(euler_heading_angle=(0, 0, 90+path_deviation_angle), 
+        path_heading_arrow_msg = get_arrow_marker_msg(euler_heading_angle=(0, 0, 90+self.path_deviation_angle), 
                                                     rgb_color=(0.0, 1.0, 0.0), 
                                                     timestamp=self.get_clock().now().to_msg(),
                                                     arrow_len=0.8, 
